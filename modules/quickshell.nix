aa10b43c3b1454672458468f5aeae2dfc3a19c5d
cother <cother@protonmail.com>
2026-07-05 14:50:46 -0700

    fix quickshell

--- modules/quickshell.nix
+++ modules/quickshell.nix
@@ -4,5 +4,6 @@
     package = inputs.quickshell.packages.${pkgs.system}.default;
     activeConfig = null;
     configs."shell.qml" = ../config/quickshell/shell.qml;
+    configs."SysMonitor.qml" = ../config/quickshell/SysMonitor.qml;
   };
 }
